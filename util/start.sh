0be9dbe7c402e9435d4311cc8d904a9f2e9ad942
Joseph Snow <josephsnow@gmail.com>
2011-11-12 21:50:00 -0700

    updated script for new repo install

--- util/start.sh
+++ util/start.sh
@@ -4,15 +4,32 @@ cd ~
 
 export PATH="/usr/local/bin:$PATH"
 
+rm -rf bc
+git clone git@github.com:finalsclubdev/bc
+cd ~/bc
+npm install connect
+npm install socket.io
+npm install express-messages
+npm install jade
+
+
 rm -rf fc
 git clone git@github.com:finalsclubdev/FinalsClub.git
 ln -sf FinalsClub fc
-cd fc
+
+cd ~/fc
 git checkout master
 git submodule init && git submodule update
 npm install
 cd etherpad-lite
 npm install
+
+# install some dependencies
+cd ~/fc
+npm install connect
+npm install socket.io
+npm install express-messages
+npm install jade
 
 
 ## init fcbackup 
@@ -21,9 +38,8 @@ chmod 775 fcbackup_init.sh
 ./fcbackup_init.sh
 
 
-
-cd
-
+## start the server
+cd ~/fc
 ./restart.sh
 
 
